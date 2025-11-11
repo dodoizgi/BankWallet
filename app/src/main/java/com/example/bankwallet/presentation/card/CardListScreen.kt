ffdf5de07764d815f8c744f3b043bdd4f53b16d3
dogusizgi <bdogusizgi@gmail.com>
2025-11-11 22:30:51 +0300

    feat: Add clipboard functionality to copy card details in CardItem

--- app/src/main/java/com/example/bankwallet/presentation/card/CardListScreen.kt
+++ app/src/main/java/com/example/bankwallet/presentation/card/CardListScreen.kt
@@ -1,38 +1,50 @@
 package com.example.bankwallet.presentation.card
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -71,11 +83,12 @@ fun CardListScreen(
 
 @Composable
 fun CardItem(card: Card, viewModel: CardViewModel) {
+    val context = LocalContext.current
     Card(
         modifier = Modifier
             .height(240.dp)
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(vertical = 4.dp, horizontal = 16.dp),
         elevation = CardDefaults.cardElevation(),
         shape = RoundedCornerShape(24.dp)
     ) {
@@ -135,7 +148,7 @@ fun CardItem(card: Card, viewModel: CardViewModel) {
 
                         Column {
                             Text(
-                                text = "İsim Soyisim",
+                                text = "Ad Soyad",
                                 color = Color(LightGray.value),
                                 fontSize = 12.sp
                             )
@@ -198,4 +211,138 @@ fun CardItem(card: Card, viewModel: CardViewModel) {
             }
         }
     }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(vertical = 4.dp, horizontal = 16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.card_background),
+            contentDescription = null,
+            modifier = Modifier
+                .matchParentSize()
+                .clip(RoundedCornerShape(12.dp)),
+            contentScale = ContentScale.Crop
+        )
+
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+            ) {
+                Button(
+                    onClick = {
+                        val clipboard =
+                            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                        val clip = ClipData.newPlainText("card_number", card.cardNumber)
+                        clipboard.setPrimaryClip(clip)
+                        Toast.makeText(context, "Kart Numarası Kopyalandı!", Toast.LENGTH_SHORT)
+                            .show()
+                    },
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight(),
+                    shape = RoundedCornerShape(0.dp),
+                    colors = buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color(White.value)
+                    )
+                ) {
+                    Text("Kart Numarasını Kopyala", fontSize = 12.sp)
+                }
+
+                VerticalDivider(
+                    modifier = Modifier.fillMaxHeight(),
+                    thickness = 1.dp,
+                    color = Color(White.value)
+                )
+
+                Button(
+                    onClick = {
+                        val clipboard =
+                            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                        val clip = ClipData.newPlainText("owner_name", card.ownerName)
+                        clipboard.setPrimaryClip(clip)
+                        Toast.makeText(context, "Ad Soyad  Kopyalandı!", Toast.LENGTH_SHORT)
+                            .show()
+                    },
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight(),
+                    shape = RoundedCornerShape(0.dp),
+                    colors = buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color(White.value)
+                    )
+                ) {
+                    Text("Ad Soyad Kopyala", fontSize = 12.sp)
+                }
+            }
+
+            HorizontalDivider(
+                modifier = Modifier.fillMaxWidth(),
+                thickness = 1.dp,
+                color = Color(White.value)
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+            ) {
+                Button(
+                    onClick = {
+                        val clipboard =
+                            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                        val clip = ClipData.newPlainText("expiration_date", card.expirationDate)
+                        clipboard.setPrimaryClip(clip)
+                        Toast.makeText(context, "Ay / Yıl Kopyalandı!", Toast.LENGTH_SHORT)
+                            .show()
+                    },
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight(),
+                    shape = RoundedCornerShape(0.dp),
+                    colors = buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color(White.value)
+                    )
+                ) {
+                    Text("Ay/Yıl Kopyala", fontSize = 12.sp)
+                }
+
+                VerticalDivider(
+                    modifier = Modifier.fillMaxHeight(),
+                    thickness = 1.dp,
+                    color = Color(White.value)
+                )
+
+                Button(
+                    onClick = {
+                        val clipboard =
+                            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                        val clip = ClipData.newPlainText("cvv", card.cvv)
+                        clipboard.setPrimaryClip(clip)
+                        Toast.makeText(context, "Cvv Kopyalandı!", Toast.LENGTH_SHORT)
+                            .show()
+                    },
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight(),
+                    shape = RoundedCornerShape(0.dp),
+                    colors = buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color(White.value)
+                    )
+                ) {
+                    Text("Cvv Kopyala", fontSize = 12.sp)
+                }
+            }
+        }
+    }
+
+    Spacer(modifier = Modifier.height(24.dp))
 }
